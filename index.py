16854e13eb71898d2351707a17f5e2fe67f2b8a4
shafien-itilite <shafien.khan@itilite.com>
2022-10-19 11:35:45 +0530

    new modified and added

--- index.py
+++ index.py
@@ -8,3 +8,12 @@ num = 8
 
 num_sqrt = num ** 0.5
 print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+
+# Python Program to convert temperature in celsius to fahrenheit
+
+# change this value for a different result
+celsius = 37.5
+
+# calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
